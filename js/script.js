1f360779a1a1649c6c1d7b774d78a2ebb3fffb9d
Anna Matviyenko and Shelby Clayton <shelby@>
2016-06-06 20:44:28 -0700

    Add functionality to our webpage.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ $(document).ready(function() {
   var answer2 = prompt("What is your favorite city?");
   var answer3 = prompt("What is your favorite cuisine?");
 
-  if (answer1 === "blond" && answer2 === "Los Angeles" || "Miami") {
-    $("#ryan").show();
+  if (answer1 === 'blond' && answer2 === 'Portland') {
+    $('#ryan').show();
+  }
+  else if (answer1 === 'brunette' && answer3 === 'indian') {
+    $('#channing').show();
+  }
+  else if (answer1 === 'black' || answer2 === 'New York') {
+    $('#brad').show();
+  }
+  else {
+    document.write("You should stay single");
   }
 });
